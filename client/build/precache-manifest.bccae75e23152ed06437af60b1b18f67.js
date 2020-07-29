c9aba04a5cc7653c224f988ed688f1f6a6740db2
delingerr <61998410+delingerr@users.noreply.github.com>
2020-07-28 21:17:03 -0400

    Add files via upload

---
+++ client/build/precache-manifest.bccae75e23152ed06437af60b1b18f67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7413e530096966fc04e4",
+    "url": "/static/css/main.b06e4800.chunk.css"
+  },
+  {
+    "revision": "7413e530096966fc04e4",
+    "url": "/static/js/main.ce8c0d89.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c7df09a61a2c37d2f3a9",
+    "url": "/static/js/2.5c25870c.chunk.js"
+  },
+  {
+    "revision": "b6a503eea0ed0a3b6f5fa51a6d3a25ed",
+    "url": "/index.html"
+  }
+];
